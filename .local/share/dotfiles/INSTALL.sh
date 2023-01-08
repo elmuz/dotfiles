b857f64a5238eb961b0c101c042cdd1de5ad0edf
Alessio Elmi <a.elmi@oxolo.com>
2023-01-08 01:32:13 +0100

    Fixed installation file

--- .local/share/dotfiles/INSTALL.sh
+++ .local/share/dotfiles/INSTALL.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+# /bin/sh
 
 # Some basic requirements
 sudo pacman --noconfirm -Syu
@@ -10,6 +10,12 @@ git clone https://github.com/elmuz/dotfiles
 cp -r dotfiles/.config dotfiles/.local .
 rm -r dotfiles
 
+# This should help keeping clean the home directory
+source .config/zsh/.zshenv
+mkdir -p $XDG_CONFIG_HOME
+mkdir -p $XDG_CACHE_HOME
+mkdir -p $XDG_DATA_HOME
+
 # Install paru in order to manage AUR packages
 mkdir tools && cd tools
 git clone https://aur.archlinux.org/paru.git
@@ -18,20 +24,22 @@ makepkg --noconfirm -si
 
 # Install required packages
 cd ~
-paru -S --needed - < ~/.local/share/dotfiles/pkglist.txt
+paru -S --needed --noconfirm - < ~/.local/share/dotfiles/pkglist.txt
+
+# Shell steroids
+sudo usermod -s /usr/bin/zsh $USER
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv .oh-my-zsh $HOME/.local/share/oh-my-zsh
+sudo sh -c 'echo "export ZDOTDIR=\$HOME/.config/zsh" > /etc/zsh/zshenv'
 
 # Fix (neo)vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # open vim and run `:PlugInstall`
 
-# Shell steroids
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp -r .oh-my-zsh $XDG_DATA_HOME/oh-my-zsh
-sudo sh -c 'echo "export ZDOTDIR=\$HOME/.config/zsh" > /etc/zsh/zshenv'
-
 # Fix keyboard
 sudo cp .local/share/X11/xkb/symbols/wasd /usr/share/X11/xkb/symbols/
 
 # Clean bash stuff
 rm -r .bash*
+
