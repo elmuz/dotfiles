d70804c19a886514e9daae6b66bf8041b73cabe9
elmuz <alessio_elmi@hotmail.com>
2024-09-12 00:14:25 +0200

    other updates in waybar and sway

--- .local/bin/backup-to-external-hdd.sh
+++ .local/bin/backup-to-external-hdd.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-
-# 1) install rsnapshot
-# 2) copy configuration file from .config/rsnapshot/rsnapshot.conf to /etc/rsnapshot.conf
-# 3) mount external hard drive to /mnt.
-# 4) create a 'backups' root folder (i.e. 'snapshot_root = /mnt/backups')
-# 5) run 'sudo rsnapshot -V daily'
+ cat << EOF
+#######################################################################################
+############################# Backup snapshots guidelines #############################
+#######################################################################################
+# 1) install rsnapshot                                                                #
+# 2) copy configuration file from .config/rsnapshot/rsnapshot.conf to                 #
+#    /etc/rsnapshot.conf                                                              #
+# 3) mount external hard drive to /mnt                                                #
+# 4) create a 'backups' root folder (e.g. 'snapshot_root = /mnt/backups')             #
+# 5) run 'sudo rsnapshot -V daily'                                                    #
+#######################################################################################
+EOF
